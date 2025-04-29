3fbaaa8ac2e45fe5488da1c03e77b702ece24db9
unknown <reyajul255@gmail.com>
2025-04-29 23:34:15 +0600

    Website homepage setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Website\IndexController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [IndexController::class, 'index']);
